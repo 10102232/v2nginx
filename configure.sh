f702acdeb1706aa7ea3ea8609abeee1474c12db2
10102232 <31153532+10102232@users.noreply.github.com>
2022-01-10 18:18:15 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-/bin/bash -c "envsubst '\$PORT' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf" && nginx -g 'daemon off;' &
+/bin/bash -c "envsubst '\$PORT' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf" && nginx -g 'daemon off;'
+
 /etc/nginx/ttyd -p 8011 -c admin:z0123456 bash
