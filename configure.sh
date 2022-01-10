0c1d5b5acd2c9f28b439409019f2803808be951c
10102232 <31153532+10102232@users.noreply.github.com>
2022-01-10 17:54:11 +0800

    1

---
+++ configure.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+nohup ./usr/bin/ttyd -p 8011 -c admin:z0123456 bash &
+/bin/bash -c "envsubst '\$PORT' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf" && nginx -g 'daemon off;'
