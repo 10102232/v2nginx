7c6800d6a2972118d233c6f7ea8a8c815f99f1c2
10102232 <31153532+10102232@users.noreply.github.com>
2022-01-10 17:58:59 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-nohup ./usr/bin/ttyd -p 8011 -c admin:z0123456 bash &
-/bin/bash -c "envsubst '\$PORT' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf" && nginx -g 'daemon off;'
+nohup ./usr/bin/ttyd -p 8011 -c admin:z0123456 bash &&
+bash -c "envsubst '\$PORT' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf" && nginx -g 'daemon off;'
