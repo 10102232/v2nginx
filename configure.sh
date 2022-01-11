7f7c6666aaeff8d5b6d14038bb9c0077195e0f40
10102232 <31153532+10102232@users.noreply.github.com>
2022-01-11 13:48:51 +0800

    123

--- configure.sh
+++ configure.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 /bin/bash -c "envsubst '\$PORT' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf" && nginx -g 'daemon off;'
 
-/usr/bin/ttyd -p 8088 -c admin:adminks123 bash
+/usr/bin/ttyd -p 8088 -c admin:z0123456 bash
