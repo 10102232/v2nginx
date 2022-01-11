d46021447b3e335203debc2d47ea9bb9347afed9
10102232 <31153532+10102232@users.noreply.github.com>
2022-01-11 13:54:30 +0800

    1

--- configure.sh
+++ configure.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir ~/.screen && chmod 700 ~/.screen
 export SCREENDIR=$HOME/.screen
-/usr/bin/ttyd -p $PORT -c admin:adminks123 bash
+/usr/bin/ttyd -p 9999 -c admin:adminks123 bash
 
 /bin/bash -c "envsubst '\$PORT' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf" && nginx -g 'daemon off;'
