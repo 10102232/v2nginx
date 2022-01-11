95c36fbde13b9de399fe9d38de3ed3e1ccaa232e
10102232 <31153532+10102232@users.noreply.github.com>
2022-01-11 12:47:33 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-/usr/bin/ttyd -p 8088 -c admin:adminks123 bash
 /bin/bash -c "envsubst '\$PORT' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf" && nginx -g 'daemon off;'
+
+/usr/bin/ttyd -p 8088 -c admin:adminks123 bash
